278107ef5d8aa29acbdb772d2ebd780a150a0df0
徐腾 <45660998+BNDS-tx@users.noreply.github.com>
2025-09-01 03:06:40 +0800

    新建项目

---
+++ NMSShipIOTool/Form1.cs
@@ -0,0 +1,14 @@
+namespace NMSShipIOTool
+{
+    using System;
+    using libNOM.io;
+    using System.Collections.Generic;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
